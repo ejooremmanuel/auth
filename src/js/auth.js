d2808193f41bafe56e99dcf15dd3f2d7e5c21ed5
Ejoor Emmanuel <55317332+ejooremmanuel@users.noreply.github.com>
2021-11-29 23:58:06 +0100

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -231,7 +231,7 @@ signUpButtonMain.addEventListener("click", async function (e) {
       password: signUpPasswordValue,
     });
     console.log(data);
-    location.href = "/src/signin-and-signup.html";
+    location.href = "/src/signin-and-signup.html?signin";
     if (data.ok) {
       // loadingCompleted();
 
